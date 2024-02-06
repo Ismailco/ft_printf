c4e4ddad1eb2fbd3996939454f37bbb0f2a6cf26
Ismail Courr <iscourr@c1r2p7.1337.ma>
2024-02-06 14:03:20 +0100

    Save the project in my repo

---
+++ ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iscourr <i_courr@hotmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 10:43:35 by iscourr           #+#    #+#             */
+/*   Updated: 2024/02/06 13:44:43 by iscourr          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <unistd.h>
+# include <stdarg.h>
+
+# include <stdio.h>
+
+int	ft_printf(const char *format, ...);
+int	ft_putstr(char *s);
+int	ft_putchar(char c);
+int	ft_putnbr(int n);
+int	ft_putnbr_unsigned(unsigned int n);
+int	ft_puthexa(unsigned int n);
+int	ft_puthexl(unsigned int n);
+
+#endif
---
+++ main.c
@@ -0,0 +1,12 @@
+// #include "ft_printf.h"
+
+// int main()
+// {
+// 	// ft_printf("%d", ft_printf("Wello %s", NULL));
+// 	// printf("%d", printf("Wello %s", NULL));
+// 	int i;
+
+//     i = 0;
+//     i = ft_printf("%X\n", 15091996);
+//     ft_printf("%d\n", i);
+// }
